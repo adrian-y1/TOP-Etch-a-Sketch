391d67f3c3c371589d663d706587c77aa70267b9
adrian-y1 <adrian.yonan1@gmail.com>
2022-08-12 20:24:07 +1000

    Improved the algorithem by creating a function that creates a grid of any size depending on the slider input. Also created a removeRow function that removes the previous grid to create a new one when user changes the size.

--- index.js
+++ index.js
@@ -2,27 +2,51 @@
 
 
 const container = document.querySelector('.container')
+const range = document.querySelector('.slider')
+const rangeLabel = document.querySelector('#slider-label')
 
+// Function to create the grid
 let counter = 0;
-
-
-for(let i=0; i < 256; i++){
-    counter++;
-    if(counter === 16){
-        const elCon = document.createElement('div')
-        elCon.className = 'row'
-        for(let j=0; j < 16; j++){
-            const el = document.createElement('div')
-            el.className = 'pixel'
-            el.onmouseover = (e) => {
-                e.target.style.background = 'black'
+function createGrid(size){
+    for(let i=0; i < size * size; i++){
+        counter++;
+        if(counter === size){
+            const elCon = document.createElement('div')
+            elCon.className = 'row'
+            for(let j=0; j < size; j++){
+                const el = document.createElement('div')
+                el.className = 'pixel'
+                el.innerHTML = j;
+                el.onmouseover = (e) => {
+                    e.target.style.background = 'black'
+                }
+                el.onmouseout = (e) => {
+                    e.target.style.background = 'black'
+                }
+                elCon.appendChild(el)
             }
-            el.onmouseout = (e) => {
-                e.target.style.background = 'black'
-            }
-            elCon.appendChild(el)
+            container.appendChild(elCon)
+            counter = 0;
         }
-        container.appendChild(elCon)
-        counter = 0;
     }
 }
+
+
+function removeRow(size){
+    const rows = container.querySelectorAll('.row')
+    rows.forEach(row => {
+        row.remove();
+    })
+}
+
+let rangeValue = parseInt(range.value);
+createGrid(rangeValue);
+
+range.oninput = (e) => {
+    rangeValue = parseInt(e.target.value)
+    removeRow(rangeValue);
+    createGrid(rangeValue);
+    rangeLabel.textContent = `${rangeValue}x${rangeValue}`;
+}
+
+
